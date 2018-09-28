1416bafeb782cef4660b6013c5ea97cac9d77a34
Caali <modsfortera@gmail.com>
2018-09-29 00:23:17 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,192 @@
+### NOTICE REGARDING ENMASSE ENTERTAINMENT (NA) TERA
+This software in this repository does not work in combination with the NA version of TERA hosted by EME (neither PC, nor PS4/XB1 servers). It contains no intellectual property belonging to them.
+
+# battle-notify
+
+A [tera-proxy](https://github.com/hackerman-caali/tera-proxy) module to show text notifications on configurable in-game events.
+
+## Preview
+
+[Click me](/previews/screenshot.png?raw=true) to view a preview of a `battle-notify` event. (Boss enraged)
+
+## Configuration
+
+The `config` folder contains all of the files necessary to customize the events that are shown.
+
+## Styling options
+
+The file `./config/common_styling.js` contains the default styling options that will be prepended to each event.
+
+These options can be overriden, per event, by using style tags in the `message` field of the event.
+
+This example shows the default style tags:
+```
+"{chat}{alert}{red}"
+```
+
+#### Notification Types
+
+- `{chat}` Display a message via the "Warning" chat channel.
+- `{alert}` Display a message in the center of the screen.
+- `{notice}` Display a message in the center of the screen, with a sound. (This option uses the party notice chat channel, but please note that the notice will only be visible on your client)
+
+#### Colors
+
+You can specify colors via style tags.
+
+![colors preview](/previews/colors.jpg?raw=true)
+
+Here is an image showing the preset color tags.
+
+You can:
+- Use preset colors. `{red}`
+- Use custom hex codes. `{#ff0000}`
+- Use multiple color tags in the same message. `{red}this text is red {blue}this text is blue`
+
+## Abnormality Events
+
+This is an example of an abnormality event:
+```
+	// Enrage Expiring, notify at 12 and 6 seconds remaining
+,	{
+		type: 'expiring',
+		target: 'MyBoss',
+		abnormalities: 8888888,
+		message: 'Enrage {duration}',
+		time_remaining: [12, 6]
+	}
+```
+
+#### type
+
+The `type` field can be any of the following (case-insensitive):
+- `'Added'` An abnormality was added to the target.
+- `'AddedOrRefreshed'` An abnormality was added, or refreshed on the target.
+- `'Refreshed'` An abnormality was refreshed on the target.
+  - Both `Refreshed` types have extra arguments: `required_stacks` (Default: 1)
+  - Specifying `required_stacks` of `5` would mean that the module only notifies you once the abnormality is refreshed with stacks of `5` or higher. This field only takes an integer as an argument.
+- `'Expiring'` An abnormality is expiring on the target.
+  - This type has extra arguments: `time_remaining` (Default: 6)
+  - Specifying a `time_remaining` of `6` would mean that when 6 seconds are left on the buff or debuff, the notification for this event is shown.
+  - Specifying a `time_remaining` of `[6, 12]`, in an array, works in a similar manner but for both 6 and 12 seconds. There is no limit on how many integers you can specify.
+- `'Removed'` An abnormality was removed from the target.
+- `'Missing'` All of the specified abnormalities are missing from the target.
+- `'MissingDuringCombat'` Similar to `Missing`, but only triggers when you are in combat.
+  - Both `Missing` types have extra arguments: `rewarn_timeout` (Default: 5)
+  - Specifying a `rewarn_timeout` of `5` would mean that you are notified every 5 seconds while the conditions for the event are met. This prevents spam. This field only takes an integer as an argument.
+
+#### target
+
+The `target` field can be any of the following (case-insensitive):
+- `'Self'` Your character.
+- `'MyBoss'` The boss that you are attacking.
+- `'Party'` Any party member, excluding yourself.
+- `'PartyIncludingSelf'` Any party member, including yourself.
+
+#### abnormalities
+
+The `abnormalities` field specifies the ID of the buff or debuff (known in the game as an `abnormality`) that you want to track.
+
+You can find a list of buffs and debuffs in the `hotdot` file for your region in the [TeraDpsMeterData](https://github.com/neowutran/TeraDpsMeterData/tree/master/hotdot) repository. [Here](https://github.com/neowutran/TeraDpsMeterData/blob/master/hotdot/hotdot-NA.tsv) is a link to the list of abnormalities for NA region. The leftmost column indicates the abnormality ID.
+
+You can specify multiple abnormalities in an array `[11111, 22222]` or a single abnormality on it's own `11111`.
+
+Since the game does not normally have an abnormality ID for enrage, it is hard-coded in the module as ID `8888888`. The module estimates how long the entity should stay enraged for, and when the next enrage should be, using the generally known 36-second rule. This is not accurate for all bosses, however, so be warned should you encounter some inaccuracies.
+
+#### message
+
+The `message` field specifies the message to be shown on the in-game notification when the event triggers. It should be a string.
+
+You can use the following text in your string and the module will replace it with the relevant information:
+- `{duration}` Display the remaining duration of the abnormality. Will not work with `'Missing'` or `'Removed'` type events. (Displays in seconds, e.g. `36s`)
+- `{stacks}` Display the number of stacks that the abnormality has on the target. (Displays an integer, e.g. `6`)
+- `{name}` The in-game name of the target that was specified.
+- `{nextEnrage}` The HP percentage that the target is expected to enrage at next. Only works with bosses. See note above in the `abnormalities` section about inaccuracies of enrage prediction. (Displays a percentage, e.g. `55%`)
+- `{icon}` The icon of the abnormality in question. (Note that Enrage does not have an icon by itself, so I am using focused niveot icon instead)
+
+#### Argument fields
+
+Extra arguments should be supplied for some event types. You can find details about them in the `type` section of this readme.
+
+## Cooldown Events
+
+Skill cooldown events should only be used in class-specific config files (i.e. anything but `config/common.js`), to avoid conflicts skill IDs between classes.
+
+Item cooldown events may be used in any config file.
+
+This is an example of a cooldown event:
+```
+	// Fire Avalanche is coming off cooldown in 10 seconds
+,	{
+		type: 'Expiring',
+		skills: 80100,
+		message: '{icon} {duration}',
+		time_remaining: 10
+	}
+```
+
+This is an example of a skill reset event:
+```
+	// Blade Draw Reset
+,	{
+		type: 'Reset',
+		skills: [290100, 370100], // normal & deadly gamble version of blade draw
+		message: '{icon} Reset'
+	}
+```
+
+#### type
+
+The `type` field for a cooldown event can be any of the following:
+- `'Expiring'` A cooldown is expiring in a specified amount of time.
+- `'ExpiringDuringCombat'` As above, plus you are in combat.
+- `'ExpiringDuringEnrage'` As above, plus the boss is enraged.
+	- All of the above `'Expiring'` type events have extra arguments: `time_remaining` (Default: 6)
+	- A `time_remaining` of `5` would mean that you are notified when 5 seconds are left on the cooldown of the skill or item.
+	- You can specify multiple values in an array `[5, 10]` to be notified at multiple times, 5 and 10 seconds in this case.
+- `'Ready'` The item or skill in question is off cooldown.
+- `'ReadyDuringCombat'` As above, plus you are in combat.
+- `'ReadyDuringEnrage'` As above, plus the boss is enraged.
+	- All of the above `'Ready'` type events have extra arguments: `rewarn_timeout` (Default: 5)
+	- A `rewarn_timeout` of `5` would mean that you are notified every 5 seconds while the conditions for the event are met. This prevents spam.
+- `'Reset'` The skill's cooldown was reset by a glyph.
+
+Please note! `Ready` type events will also be triggered by skill resets. There should be no cases in which you need a `Ready` event and a `Reset` event for the same skill. Should you configure it as such, you would be notified twice when a skill reset happens.
+
+#### skills
+
+The `skills` field for a skill reset event specifies the skill IDs that you would like to track.
+
+You can find a list of skills in the `skills` file for your region in the [TeraDpsMeterData](https://github.com/neowutran/TeraDpsMeterData/blob/master/skills/) repository. [Here](https://github.com/neowutran/TeraDpsMeterData/blob/master/skills/skills-NA.tsv) is a link to the list of skills for NA region. The leftmost column indicates the skill ID.
+
+You can specify a single skill ID `290100` or multiple skill IDs in an array `[290100, 370100]`.
+
+Skills trigger an event by their respective group, rather than the raw skill ID that you supply. So, `290100` would hook group `29`. This is done so that you do not need to supply the skill ID for each level of the skill. However, in some cases a skill has a different group in certain conditions (See warrior blade draw with & without deadly gamble buff, or slayer OHS with ICB). In these cases you must provide the base skill ID and the buffed skill ID, as shown above for warrior's blade draw.
+
+#### items
+
+The `items` field specifies the item ID(s) that you want to track.
+
+You can find a list of item IDs at this website, under `Database -> Items`: [TeraDatabase](http://teradatabase.net) (note: You should *not* use this site to obtain skill IDs. Only use this site for item IDs).
+
+You can specify a single item ID `98267` or multiple item IDs in an array `[98267, 98260]`.
+
+You may specify items and skills under the same event, but they must still be separated by their respective types.
+```
+	skills: 80100,
+	items: [98267, 98260]
+```
+
+When you specify multiple skills, items, or both, they will be processed individually. This means that the script will not wait for *all* of the skills/items to meet the conditions that you specify, it will check if each of them meets the condition individually and notify you as such.
+
+#### message
+
+The `message` field specifies the message to be shown on the in-game notification when the event triggers. It should be a string.
+
+You can use the following text in your string and the module will replace it with the relevant information:
+- `{duration}` Display the time remaining on the cooldown, in seconds. (e.g. `5s`)
+- `{icon}` Display the icon of the skill or item in question.
+
+## Planned Features
+- Boss mechanic events (e.g. P3 Vergos debuffs)
+- In-game commands (on, off, ...)
---
+++ index.js
@@ -0,0 +1,380 @@
+const debug = false
+const AbnormalManager = tryRequire('./lib/abnormal')
+const CooldownManager = tryRequire('./lib/cooldown')
+const EntityManager = tryRequire('./lib/entity')
+const PartyManager = tryRequire('./lib/party')
+const Notify = tryRequire('./lib/notify')
+
+const isDefined = x => typeof x !== 'undefined'
+const isArray = x => x instanceof Array
+const isError = x => x instanceof Error
+const toArray = x => isArray(x) ? x : (isDefined(x) ? [x] : [])
+const toSet = x => new Set(toArray(x))
+const thisIfGreater = (x, y) => (x > y) ? x : false
+const thisIfSmaller = (x, y) => (x < y) ? x : false
+const skillGroup = x => Math.floor(x / 10000)
+const msRemaining = uts => uts - Date.now()
+const sRemaining = uts => Math.round(msRemaining(uts) / 1000)
+const matchExpiring = (set, uts) => set.has(sRemaining(uts))
+
+for (const lib of [AbnormalManager, CooldownManager, EntityManager, PartyManager, Notify]){
+    if(!lib) return
+}
+
+function tryIt(func) {
+    try {
+        return func()
+    } catch (e) {
+        return e
+    }
+}
+
+function tryRequire(path){
+    try {
+        delete require.cache[require.resolve(path)]
+        return require(path)
+    } catch (e) {
+        logError([
+            `[battle-notify] require: error loading (${path})`,
+            e.stack
+        ])
+    }
+}
+
+function logError(message) {
+    console.error(Array.isArray(message) ? message.join('\n') : message)
+}
+
+module.exports = function BattleNotify(mod){
+    let enabled = false
+    const abMan = new AbnormalManager(mod, debug)
+    const cooldown = new CooldownManager(mod, debug)
+    const entities = new EntityManager(mod, debug)
+    const party = new PartyManager(mod, debug)
+    const notify = new Notify(mod, debug)
+    const conditions = new Conditions()
+    const targets = new Targets()
+    const events = new Set()
+
+    const combat = () => entities.myEntity().combat
+    const enrage = () => entities.myBoss().enraged
+
+    mod.hook('S_LOGIN', 10, (event) => {
+        enabled = true
+        refreshConfig()
+    })
+    mod.hook('S_RETURN_TO_LOBBY', 'raw', (data) => {
+        enabled = false
+    })
+    mod.hook('S_PRIVATE_CHAT', 1, (event) => {
+        if(!debug) return
+        enabled = true
+        setTimeout(refreshConfig, 5)
+    })
+
+    function Conditions(){
+        function AbnormalConditions(){
+            const checkAdded = (lastMatch, {added} = {}) => added
+            const checkRemoved = (lastMatch, {removed} = {}) => removed
+
+            function AddedOrRefreshed({requiredStacks} = {}){
+                this.requiredStacks = requiredStacks
+                return checkAddedOrRefreshed.bind(this)
+            }
+            function checkAddedOrRefreshed(lastMatch, {stacks = 0, added, refreshed} = {}) {
+                if(stacks > this.requiredStacks)
+                    return refreshed || added
+            }
+
+            function Refreshed({requiredStacks} = {}){
+                this.requiredStacks = requiredStacks
+                return checkRefreshed.bind(this)
+            }
+            function checkRefreshed(lastMatch, {stacks = 0, refreshed, added} = {}) {
+                if(stacks > this.requiredStacks)
+                    return refreshed || added
+            }
+
+            function Expiring({timesToMatch} = {}) {
+                this.timesToMatch = timesToMatch
+                return checkExpiring.bind(this)
+            }
+            function checkExpiring(lastMatch, {expires = 0, added, refreshed} = {}){
+                if(matchExpiring(this.timesToMatch, expires))
+                    return (refreshed || added) + sRemaining(expires)
+            }
+
+            function Missing({rewarnTimeout} = {}){
+                this.rewarnTimeout = rewarnTimeout * 1000
+                return checkMissing.bind(this)
+            }
+            function checkMissing(lastMatch, {added, refreshed} = {}) {
+                if(added || refreshed) return
+                return thisIfGreater(Date.now(), lastMatch + this.rewarnTimeout)
+            }
+
+            function MissingDuringCombat({rewarnTimeout} = {}){
+                this.rewarnTimeout = rewarnTimeout * 1000
+                return checkMissingDuringCombat.bind(this)
+            }
+            function checkMissingDuringCombat(lastMatch, {added, refreshed} = {}) {
+                if(added || refreshed || !combat()) return
+                return thisIfGreater(Date.now(), lastMatch + this.rewarnTimeout)
+            }
+
+            this.added = (x) => checkAdded
+            this.removed = (x) => checkRemoved
+            this.addedorrefreshed = (x) => new AddedOrRefreshed(x)
+            this.refreshed = (x) => new Refreshed(x)
+            this.expiring = (x) => new Expiring(x)
+            this.missing = (x) => new Missing(x)
+            this.missingduringcombat = (x) => new MissingDuringCombat(x)
+        }
+
+        function CooldownConditions(){
+            
+            function Expiring({timesToMatch} = {}){
+                this.timesToMatch = timesToMatch
+                return checkExpiring.bind(this)
+            }
+            function checkExpiring(lastMatch, {expires} = {}){
+                if(matchExpiring(this.timesToMatch, expires))
+                    return expires - sRemaining(expires)
+            }
+
+            function ExpiringDuringCombat({timesToMatch} = {}){
+                this.timesToMatch = timesToMatch
+                return checkExpiringDuringCombat.bind(this)
+            }
+            function checkExpiringDuringCombat(lastMatch, {expires = 0} = {}){
+                if(combat())
+                    return checkExpiring.call(this, ...arguments)
+            }
+
+            function ExpiringDuringEnrage({timesToMatch} = {}){
+                this.timesToMatch = timesToMatch
+                return checkExpiringDuringEnrage.bind(this)
+            }
+            function checkExpiringDuringEnrage(lastMatch, {expires = 0} = {}){
+                if(enrage)
+                    return checkExpiringDuringCombat.call(this, ...arguments)
+            }
+
+            function Ready({rewarnTimeout} = {}){
+                this.rewarnTimeout = rewarnTimeout * 1000
+                return checkReady.bind(this)
+            }
+            function checkReady(lastMatch, {expires = 0} = {}){
+                if(Date.now() > expires)
+                    return thisIfGreater(Date.now(), lastMatch + this.rewarnTimeout)
+            }
+
+            function ReadyDuringCombat({rewarnTimeout} = {}){
+                this.rewarnTimeout = rewarnTimeout * 1000
+                return checkReadyDuringCombat.bind(this)
+            }
+            function checkReadyDuringCombat(lastMatch, {expires = 0} = {}){
+                if(combat())
+                    return checkReady.call(this, ...arguments)
+            }
+
+            function ReadyDuringEnrage({rewarnTimeout} = {}){
+                this.rewarnTimeout = rewarnTimeout * 1000
+                return checkReadyDuringEnrage.bind(this)
+            }
+            function checkReadyDuringEnrage (lastMatch, {expires = 0} = {}){
+                if(enrage())
+                    return checkReadyDuringCombat.call(this, ...arguments)
+            }
+
+            this.expiring = (x) => new Expiring(x)
+            this.expiringduringcombat = (x) => new ExpiringDuringCombat(x)
+            this.expiringduringenrage = (x) => new ExpiringDuringEnrage(x)
+            this.ready = (x) => new Ready(x)
+            this.readyduringcombat = (x) => new ReadyDuringCombat(x)
+            this.readyduringenrage = (x) => new ReadyDuringEnrage(x)
+        }
+
+        this.cooldown = new CooldownConditions()
+        this.abnormal = new AbnormalConditions()
+    }
+
+    function Targets(){
+        function AbnormalTargets(){
+            this.self = () => [entities.myCid()]
+            this.myboss = () => [entities.myBossId()]
+            this.party = () => party.members()
+                .filter(cid => cid !== entities.myCid())
+            this.partyincludingself = () => party.members()
+        }
+        function CooldownTargets(skills, items){
+            skills = Array.from(skills)
+            items = Array.from(items)
+            return () =>
+                skills.map(id => cooldown.skill(id))
+                    .concat(items.map(id => cooldown.item(id)))
+        }
+        this.cooldown = CooldownTargets
+        this.abnormal = new AbnormalTargets()
+    }
+
+    function AbnormalEvent(data){
+        const type = data.type.toLowerCase()
+        const target = data.target.toLowerCase()
+        const getTargets = targets.abnormal[target]
+        const event = {}
+        const args = event.args = {
+            timesToMatch: toSet(data.time_remaining || 6),
+            rewarnTimeout: data.rewarn_timeout || 5,
+            requiredStacks: data.required_stacks || 1
+        }
+        event.abnormalities = toSet(data.abnormalities)
+        event.condition = conditions.abnormal[type](args)
+        event.message = data.message
+        event.lastMatches = new Map()
+        event.matchAll = type.includes('missing')
+
+        this.check = function(){
+            getTargets()
+                .map(id => tryIt(() => checkAbnormalEvent(id, event)))
+                .filter(isError)
+                .forEach(err => logError([
+                    `[battle-notify] AbnormalEvent.check: error while checking event`,
+                    `event: ${JSON.stringify(event || {})}`,
+                    err.stack
+                ]))
+        }
+    }
+    function checkAbnormalEvent(entityId, event){
+        if(!entityId) return
+        const entity = entities.get(entityId)
+        if(entity.dead) return
+
+        entityId = entityId.toString()
+        if(!event.lastMatches.has(entityId))
+            event.lastMatches.set(entityId, 0)
+
+        const results = new Set()
+        let info
+        let currentMatch
+
+        for(const abnormal of event.abnormalities){
+            const lastMatch = event.lastMatches.get(entityId)
+            const abnormalInfo = abMan.get(entityId, abnormal)
+            const match = event.condition(lastMatch, abnormalInfo)
+
+            if(match && match !== lastMatch){
+                currentMatch = match
+                info = abnormalInfo
+                results.add(true)
+            } else results.add(false)
+        }
+
+        if(event.matchAll && results.has(false) || !results.has(true)) return
+        notify.abnormal(event.message, entity, info)
+        event.lastMatches.set(entityId, currentMatch)
+    }
+
+    function CooldownEvent(data){
+        data.skills = toArray(data.skills)
+        data.items = toArray(data.items)
+        const type = data.type.toLowerCase()
+        const getTargets = targets.cooldown(data.skills, data.items)
+        const event = {}
+        const args = event.args = {
+            timesToMatch: toSet(data.time_remaining || 6),
+            rewarnTimeout: data.rewarn_timeout || 5
+        }
+        event.condition = conditions.cooldown[type](args)
+        event.message = data.message
+        event.lastMatches = new Map()
+
+        this.check = function(){
+            getTargets()
+                .map(info =>
+                    tryIt(() => checkCooldownEvent(info, event)))
+                .filter(isError)
+                .forEach(err => logError([
+                    `[battle-notify] CooldownEvent.check: error while checking event`,
+                    `event: ${JSON.stringify(event || {})}`,
+                    err.stack
+                ]))
+        }
+    }
+    function checkCooldownEvent(info, event){
+        const id = info.item ? info.item : info.skill
+        if(!event.lastMatches.has(id))
+            event.lastMatches.set(id, 0)
+
+        const lastMatch = event.lastMatches.get(id)
+        const match = event.condition(lastMatch, info)
+        if(match && match !== lastMatch){
+            notify.cooldown(event.message, info)
+            event.lastMatches.set(id, match)
+        }
+    }
+
+    function ResetEvent(data){
+        cooldown.onReset(toArray(data.skills), info => {
+            notify.skillReset(data.message, info)
+        })
+        this.check = function(){}
+    }
+
+    function refreshConfig(){
+        events.clear()
+        cooldown.clearResetHooks()
+
+        loadEvents('./config/' + entities.self().class)
+        loadEvents('./config/common')
+
+        loadStyling('./config/common_styling.js')
+    }
+    function loadStyling(path){
+        const data = tryRequire(path)
+        if(!data) return
+        notify.setDefaults(data)
+    }
+    function loadEvent(event){
+        let type
+        if (event.abnormalities)
+            type = AbnormalEvent
+        else if (event.type && event.type.toLowerCase() === 'reset')
+            type = ResetEvent
+        else if (event.skills || event.items)
+            type = CooldownEvent
+
+        return new type(event)
+    }
+    function loadEvents(path){
+        const data = tryRequire(path)
+
+        toArray(data)
+            .forEach(event => {
+                const result = tryIt(() => loadEvent(event))
+
+                if(isError(result)){
+                    logError([
+                        `[battle-notify] loadEvents error while loading event from ${path}`,
+                        `event: ${JSON.stringify(event)}`,
+                        result.stack
+                    ])
+                    return
+                }
+                events.add(result)
+            })
+    }
+    function checkEvents(){
+        if(!enabled) return
+        events.forEach(e => e.check())
+    }
+    const checkTimer = setInterval(checkEvents, 500)
+    this.destructor = function(){
+        clearInterval(checkTimer)
+    }
+    if(debug) {
+        mod.send('C_CHAT', 1, {"channel":11,"message":"<FONT></FONT>"})
+
+        //notify.testColors()
+    }
+}
