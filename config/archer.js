bb3c27a410870740f0dd5673c88c645f961ceafe
Caali <modsfortera@gmail.com>
2019-09-08 21:08:21 +0200

    Remove archer skill that doesn't exist anymore from default config.

--- config/archer.js
+++ config/archer.js
@@ -1,10 +1,3 @@
 module.exports = [
-	// Velik's Mark
-	{
-		type: 'MissingDuringCombat',
-		target: 'MyBoss',
-		abnormalities: [600502, 600501, 600500],
-		message: "Missing Velik's Mark",
-		rewarn_timeout: 10
-	}
+
 ]
