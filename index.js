dd096df3963166e48a5f319dac39ee78461e487d
Risenio <Risenio@users.noreply.github.com>
2019-10-16 15:47:47 +0400

    Fixes.
    Sorry for the formatting, can't help it.
    Solves #1 , #2 , #3  (Fixed the issue where going to lobby would prevent the module from functioning no longer).
    Removed unused functions.

--- index.js
+++ index.js
@@ -1,58 +1,44 @@
-module.exports = function BattleNotify(mod){
-    const debug = false
-    const AbnormalManager = tryRequire('./lib/abnormal')
-    const CooldownManager = tryRequire('./lib/cooldown')
-    const EntityManager = tryRequire('./lib/entity')
-    const PartyManager = tryRequire('./lib/party')
-    const Notify = tryRequire('./lib/notify')
+'use strict'
 
-    const isDefined = x => typeof x !== 'undefined'
-    const isArray = x => x instanceof Array
-    const isError = x => x instanceof Error
-    const toArray = x => isArray(x) ? x : (isDefined(x) ? [x] : [])
-    const toSet = x => new Set(toArray(x))
-    const thisIfGreater = (x, y) => (x > y) ? x : false
-    const thisIfSmaller = (x, y) => (x < y) ? x : false
-    const skillGroup = x => Math.floor(x / 10000)
-    const msRemaining = uts => uts - Date.now()
-    const sRemaining = uts => Math.round(msRemaining(uts) / 1000)
-    const matchExpiring = (set, uts) => set.has(sRemaining(uts))
+const enabled = true
+const debug = false
+const AbnormalManager = require('./lib/abnormal')
+const CooldownManager = require('./lib/cooldown')
+const EntityManager = require('./lib/entity')
+const PartyManager = require('./lib/party')
+const Notify = require('./lib/notify')
 
-    for (const lib of [AbnormalManager, CooldownManager, EntityManager, PartyManager, Notify]){
-        if(!lib) return
+const isDefined = x => typeof x !== 'undefined'
+const isArray = x => x instanceof Array
+const isError = x => x instanceof Error
+const toArray = x => isArray(x) ? x : (isDefined(x) ? [x] : [])
+const toSet = x => new Set(toArray(x))
+const thisIfGreater = (x, y) => (x > y) ? x : false
+//const thisIfSmaller = (x, y) => (x < y) ? x : false
+//const skillGroup = x => Math.floor(x / 10000)
+const msRemaining = uts => uts - Date.now()
+const sRemaining = uts => Math.round(msRemaining(uts) / 1000)
+const matchExpiring = (set, uts) => set.has(sRemaining(uts))
+
+function tryIt(func) {
+    try {
+        return func()
+    } catch (e) {
+        return e
     }
+}
 
-    function tryIt(func) {
-        try {
-            return func()
-        } catch (e) {
-            return e
-        }
-    }
+function logError(message) {
+    console.error(Array.isArray(message) ? message.join('\n') : message)
+}
 
-    function tryRequire(path){
-        try {
-            delete require.cache[require.resolve(path)]
-            return require(path)
-        } catch (e) {
-            logError([
-                `[battle-notify] require: error loading (${path})`,
-                e.stack
-            ])
-        }
-    }
+function BattleNotify(mod) {
 
-    function logError(message) {
-        mod.error(Array.isArray(message) ? message.join('\n') : message)
-    }
-
-
-    let enabled = false
-    const abMan = new AbnormalManager(mod, debug)
-    const cooldown = new CooldownManager(mod, debug)
+    const abMan = new AbnormalManager(mod)
+    const cooldown = new CooldownManager(mod)
     const entities = new EntityManager(mod, debug)
-    const party = new PartyManager(mod, debug)
-    const notify = new Notify(mod, debug)
+    const party = new PartyManager(mod)
+    const notify = new Notify(mod)
     const conditions = new Conditions()
     const targets = new Targets()
     const events = new Set()
@@ -60,66 +46,69 @@ module.exports = function BattleNotify(mod){
     const combat = () => entities.myEntity().combat
     const enrage = () => entities.myBoss().enraged
 
+    let interval = null
+
     mod.hook('S_LOGIN', 'event', () => {
-        enabled = true
-        refreshConfig()
+        if (enabled) {
+            interval = mod.setInterval(checkEvents, 500)
+            refreshConfig()
+        }
     })
     mod.hook('S_RETURN_TO_LOBBY', 'event', () => {
-        enabled = false
+        mod.clearInterval(interval)
     })
     mod.hook('S_PRIVATE_CHAT', 'event', () => {
-        if(!debug) return
-        enabled = true
+        if (!debug) return
         mod.setTimeout(refreshConfig, 5)
     })
 
-    function Conditions(){
-        function AbnormalConditions(){
-            const checkAdded = (lastMatch, {added} = {}) => added
-            const checkRemoved = (lastMatch, {removed} = {}) => removed
+    function Conditions() {
+        function AbnormalConditions() {
+            const checkAdded = (lastMatch, { added } = {}) => added
+            const checkRemoved = (lastMatch, { removed } = {}) => removed
 
-            function AddedOrRefreshed({requiredStacks} = {}){
+            function AddedOrRefreshed({ requiredStacks } = {}) {
                 this.requiredStacks = requiredStacks
                 return checkAddedOrRefreshed.bind(this)
             }
-            function checkAddedOrRefreshed(lastMatch, {stacks = 0, added, refreshed} = {}) {
-                if(stacks > this.requiredStacks)
+            function checkAddedOrRefreshed(lastMatch, { stacks = 0, added, refreshed } = {}) {
+                if (stacks > this.requiredStacks)
                     return refreshed || added
             }
 
-            function Refreshed({requiredStacks} = {}){
+            function Refreshed({ requiredStacks } = {}) {
                 this.requiredStacks = requiredStacks
                 return checkRefreshed.bind(this)
             }
-            function checkRefreshed(lastMatch, {stacks = 0, refreshed, added} = {}) {
-                if(stacks > this.requiredStacks)
+            function checkRefreshed(lastMatch, { stacks = 0, refreshed, added } = {}) {
+                if (stacks > this.requiredStacks)
                     return refreshed || added
             }
 
-            function Expiring({timesToMatch} = {}) {
+            function Expiring({ timesToMatch } = {}) {
                 this.timesToMatch = timesToMatch
                 return checkExpiring.bind(this)
             }
-            function checkExpiring(lastMatch, {expires = 0, added, refreshed} = {}){
-                if(matchExpiring(this.timesToMatch, expires))
+            function checkExpiring(lastMatch, { expires = 0, added, refreshed } = {}) {
+                if (matchExpiring(this.timesToMatch, expires))
                     return (refreshed || added) + sRemaining(expires)
             }
 
-            function Missing({rewarnTimeout} = {}){
+            function Missing({ rewarnTimeout } = {}) {
                 this.rewarnTimeout = rewarnTimeout * 1000
                 return checkMissing.bind(this)
             }
-            function checkMissing(lastMatch, {added, refreshed} = {}) {
-                if(added || refreshed) return
+            function checkMissing(lastMatch, { added, refreshed } = {}) {
+                if (added || refreshed) return
                 return thisIfGreater(Date.now(), lastMatch + this.rewarnTimeout)
             }
 
-            function MissingDuringCombat({rewarnTimeout} = {}){
+            function MissingDuringCombat({ rewarnTimeout } = {}) {
                 this.rewarnTimeout = rewarnTimeout * 1000
                 return checkMissingDuringCombat.bind(this)
             }
-            function checkMissingDuringCombat(lastMatch, {added, refreshed} = {}) {
-                if(added || refreshed || !combat()) return
+            function checkMissingDuringCombat(lastMatch, { added, refreshed } = {}) {
+                if (added || refreshed || !combat()) return
                 return thisIfGreater(Date.now(), lastMatch + this.rewarnTimeout)
             }
 
@@ -132,59 +121,59 @@ module.exports = function BattleNotify(mod){
             this.missingduringcombat = (x) => new MissingDuringCombat(x)
         }
 
-        function CooldownConditions(){
+        function CooldownConditions() {
 
-            function Expiring({timesToMatch} = {}){
+            function Expiring({ timesToMatch } = {}) {
                 this.timesToMatch = timesToMatch
                 return checkExpiring.bind(this)
             }
-            function checkExpiring(lastMatch, {expires} = {}){
-                if(matchExpiring(this.timesToMatch, expires))
+            function checkExpiring(lastMatch, { expires } = {}) {
+                if (matchExpiring(this.timesToMatch, expires))
                     return expires - sRemaining(expires)
             }
 
-            function ExpiringDuringCombat({timesToMatch} = {}){
+            function ExpiringDuringCombat({ timesToMatch } = {}) {
                 this.timesToMatch = timesToMatch
                 return checkExpiringDuringCombat.bind(this)
             }
-            function checkExpiringDuringCombat(lastMatch, {expires = 0} = {}){
-                if(combat())
+            function checkExpiringDuringCombat(lastMatch, { expires = 0 } = {}) {
+                if (combat())
                     return checkExpiring.call(this, ...arguments)
             }
 
-            function ExpiringDuringEnrage({timesToMatch} = {}){
+            function ExpiringDuringEnrage({ timesToMatch } = {}) {
                 this.timesToMatch = timesToMatch
                 return checkExpiringDuringEnrage.bind(this)
             }
-            function checkExpiringDuringEnrage(lastMatch, {expires = 0} = {}){
-                if(enrage)
+            function checkExpiringDuringEnrage(lastMatch, { expires = 0 } = {}) {
+                if (enrage)
                     return checkExpiringDuringCombat.call(this, ...arguments)
             }
 
-            function Ready({rewarnTimeout} = {}){
+            function Ready({ rewarnTimeout } = {}) {
                 this.rewarnTimeout = rewarnTimeout * 1000
                 return checkReady.bind(this)
             }
-            function checkReady(lastMatch, {expires = 0} = {}){
-                if(Date.now() > expires)
+            function checkReady(lastMatch, { expires = 0 } = {}) {
+                if (Date.now() > expires)
                     return thisIfGreater(Date.now(), lastMatch + this.rewarnTimeout)
             }
 
-            function ReadyDuringCombat({rewarnTimeout} = {}){
+            function ReadyDuringCombat({ rewarnTimeout } = {}) {
                 this.rewarnTimeout = rewarnTimeout * 1000
                 return checkReadyDuringCombat.bind(this)
             }
-            function checkReadyDuringCombat(lastMatch, {expires = 0} = {}){
-                if(combat())
+            function checkReadyDuringCombat(lastMatch, { expires = 0 } = {}) {
+                if (combat())
                     return checkReady.call(this, ...arguments)
             }
 
-            function ReadyDuringEnrage({rewarnTimeout} = {}){
+            function ReadyDuringEnrage({ rewarnTimeout } = {}) {
                 this.rewarnTimeout = rewarnTimeout * 1000
                 return checkReadyDuringEnrage.bind(this)
             }
-            function checkReadyDuringEnrage (lastMatch, {expires = 0} = {}){
-                if(enrage())
+            function checkReadyDuringEnrage(lastMatch, { expires = 0 } = {}) {
+                if (enrage())
                     return checkReadyDuringCombat.call(this, ...arguments)
             }
 
@@ -200,15 +189,15 @@ module.exports = function BattleNotify(mod){
         this.abnormal = new AbnormalConditions()
     }
 
-    function Targets(){
-        function AbnormalTargets(){
+    function Targets() {
+        function AbnormalTargets() {
             this.self = () => [entities.myCid()]
             this.myboss = () => [entities.myBossId()]
             this.party = () => party.members()
                 .filter(cid => cid !== entities.myCid())
             this.partyincludingself = () => party.members()
         }
-        function CooldownTargets(skills, items){
+        function CooldownTargets(skills, items) {
             skills = Array.from(skills)
             items = Array.from(items)
             return () =>
@@ -219,7 +208,7 @@ module.exports = function BattleNotify(mod){
         this.abnormal = new AbnormalTargets()
     }
 
-    function AbnormalEvent(data){
+    function AbnormalEvent(data) {
         const type = data.type.toLowerCase()
         const target = data.target.toLowerCase()
         const getTargets = targets.abnormal[target]
@@ -235,7 +224,7 @@ module.exports = function BattleNotify(mod){
         event.lastMatches = new Map()
         event.matchAll = type.includes('missing')
 
-        this.check = function(){
+        this.check = function () {
             getTargets()
                 .map(id => tryIt(() => checkAbnormalEvent(id, event)))
                 .filter(isError)
@@ -246,37 +235,37 @@ module.exports = function BattleNotify(mod){
                 ]))
         }
     }
-    function checkAbnormalEvent(entityId, event){
-        if(!entityId) return
+    function checkAbnormalEvent(entityId, event) {
+        if (!entityId) return
         const entity = entities.get(entityId)
-        if(entity.dead) return
+        if (entity.dead) return
 
         entityId = entityId.toString()
-        if(!event.lastMatches.has(entityId))
+        if (!event.lastMatches.has(entityId))
             event.lastMatches.set(entityId, 0)
 
         const results = new Set()
         let info
         let currentMatch
 
-        for(const abnormal of event.abnormalities){
+        for (const abnormal of event.abnormalities) {
             const lastMatch = event.lastMatches.get(entityId)
             const abnormalInfo = abMan.get(entityId, abnormal)
             const match = event.condition(lastMatch, abnormalInfo)
 
-            if(match && match !== lastMatch){
+            if (match && match !== lastMatch) {
                 currentMatch = match
                 info = abnormalInfo
                 results.add(true)
             } else results.add(false)
         }
 
-        if(event.matchAll && results.has(false) || !results.has(true)) return
+        if (event.matchAll && results.has(false) || !results.has(true)) return
         notify.abnormal(event.message, entity, info)
         event.lastMatches.set(entityId, currentMatch)
     }
 
-    function CooldownEvent(data){
+    function CooldownEvent(data) {
         data.skills = toArray(data.skills)
         data.items = toArray(data.items)
         const type = data.type.toLowerCase()
@@ -290,7 +279,7 @@ module.exports = function BattleNotify(mod){
         event.message = data.message
         event.lastMatches = new Map()
 
-        this.check = function(){
+        this.check = function () {
             getTargets()
                 .map(info =>
                     tryIt(() => checkCooldownEvent(info, event)))
@@ -302,27 +291,27 @@ module.exports = function BattleNotify(mod){
                 ]))
         }
     }
-    function checkCooldownEvent(info, event){
+    function checkCooldownEvent(info, event) {
         const id = info.item ? info.item : info.skill
-        if(!event.lastMatches.has(id))
+        if (!event.lastMatches.has(id))
             event.lastMatches.set(id, 0)
 
         const lastMatch = event.lastMatches.get(id)
         const match = event.condition(lastMatch, info)
-        if(match && match !== lastMatch){
+        if (match && match !== lastMatch) {
             notify.cooldown(event.message, info)
             event.lastMatches.set(id, match)
         }
     }
 
-    function ResetEvent(data){
+    function ResetEvent(data) {
         cooldown.onReset(toArray(data.skills), info => {
             notify.skillReset(data.message, info)
         })
-        this.check = function(){}
+        this.check = function () { }
     }
 
-    function refreshConfig(){
+    function refreshConfig() {
         events.clear()
         cooldown.clearResetHooks()
 
@@ -331,12 +320,12 @@ module.exports = function BattleNotify(mod){
 
         loadStyling('./config/common_styling.js')
     }
-    function loadStyling(path){
-        const data = tryRequire(path)
-        if(!data) return
+    function loadStyling(path) {
+        const data = require(path)
+        if (!data) return
         notify.setDefaults(data)
     }
-    function loadEvent(event){
+    function loadEvent(event) {
         let type
         if (event.abnormalities)
             type = AbnormalEvent
@@ -347,14 +336,14 @@ module.exports = function BattleNotify(mod){
 
         return new type(event)
     }
-    function loadEvents(path){
-        const data = tryRequire(path)
+    function loadEvents(path) {
+        const data = require(path)
 
         toArray(data)
             .forEach(event => {
                 const result = tryIt(() => loadEvent(event))
 
-                if(isError(result)){
+                if (isError(result)) {
                     logError([
                         `[battle-notify] loadEvents error while loading event from ${path}`,
                         `event: ${JSON.stringify(event)}`,
@@ -365,16 +354,16 @@ module.exports = function BattleNotify(mod){
                 events.add(result)
             })
     }
-    function checkEvents(){
-        if(!enabled) return
+    function checkEvents() {
         events.forEach(e => e.check())
     }
 
-    mod.setInterval(checkEvents, 500)
-
-    if(debug) {
-        mod.send('C_CHAT', 1, {"channel":11,"message":"<FONT></FONT>"})
+    if (debug) {
+        mod.send('C_CHAT', 1, { "channel": 11, "message": "<FONT></FONT>" })
 
         //notify.testColors()
     }
 }
+
+
+module.exports = BattleNotify;
